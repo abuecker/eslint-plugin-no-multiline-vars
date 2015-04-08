2578b2a2737c9afb040806f047d8a88bb1048d2d
Andy Buecker <andy@tower3.io>
2015-04-07 22:57:55 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+    rules: {
+        'no-multiline-vars': require('./lib/rules/no-multiline-vars')
+    },
+    rulesConfig: {
+        'no-multiline-vars': 2
+    }
+}
